2791a72b5511358e5ce41056df55e497d43fe3a1
Joachimmoller <joachimmoller@gmail.com>
2024-03-18 20:59:38 +0100

    + Add Artist Modal

--- reacterontend/src/App.js
+++ reacterontend/src/App.js
@@ -1,45 +1,74 @@
 import Artist from "./Components/Artist";
 import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import NewArtist from "./Components/NewArtist";
+import AddArtist from "./Components/AddArtist";
 
 function App() {
+    
   const [genre, setGenre] = useState("Rock");
   const [artists, setArtists] = useState(
       [
-        {name: "Metallica", genre: "Thrash Metal", img: ""},
-        {name: "Emperor", genre: "Black Metal", img: ""},
-        {name: "Pantera", genre: "Groove Metal", img: ""},
-        {name: "Samael", genre: "Industrial Black Metal", img: ""},
-        {name: "Opeth", genre: "Progressive Metal", img: ""},
-        {name: "Tool", genre: "Progressive Metal", img: ""}
+        {id: 1, name: "Metallica", genre: "Thrash Metal", img: ""},
+        {id: 2, name: "Emperor", genre: "Black Metal", img: ""},
+        {id: 3, name: "Pantera", genre: "Groove Metal", img: ""},
+        {id: 4, name: "Samael", genre: "Industrial Black Metal", img: ""},
+        {id: 5, name: "Opeth", genre: "Progressive Metal", img: ""},
+        {id: 6, name: "Tool", genre: "Progressive Metal", img: ""}
       ]
   );
+  function updateArtist(id, newName, newGenre) {
+      const updatedArtists = artists.map(artist => {
+          if(id == artist.id) {
+              return {...artist, name: newName, genre: newGenre};
+          }
+          return artist;
+      });
+          setArtists(updatedArtists);
+      
+  }
+  
+  function newArtist(name, genre) {
+      newArtist = {
+          id: uuidv4(),
+          name: name,
+          genre: genre,
+          img: ""
+      };
+      setArtists([...artists, newArtist]);
+  }
   const showArtist = true;
   return (
-    <div className="App">
-      {showArtist ? 
-      <>
-        <input type="text" onChange={(e) => {
-            console.log(e.target.value);
-            setGenre(e.target.value);
-          }}
-          />
-          <NewArtist />
-        <div className="flex flex-wrap justify-center">
-            {artists.map((artist, index) => {
-                return (
-                <Artist key={uuidv4()} name={artist.name} genre={artist.genre} img={artist.img}
-                />
-                );
-            })}
-        </div>
-      </>
-          : 
+    <div className="App bg-red-400 min-h-[960px]">
+      {showArtist ?
+          <>
+              <input type="text" onChange={(e) => {
+                  console.log(e.target.value);
+                  setGenre(e.target.value);
+              }}
+              />
+              <p>TODO: Create Header</p>
+              <p>TODO: Create Background</p>
+              <AddArtist newArtist={newArtist}/>
+              <div className="flex flex-wrap justify-center">
+                  {artists.map((artist, index) => {
+                      return (
+                          <Artist key={artist.id}
+                                  id={artist.id}
+                                  name={artist.name}
+                                  genre={artist.genre}
+                                  img={artist.img}
+                                  updateArtist={updateArtist}
+                          />
+                      );
+                  })}
+              </div>
+          </>
+          :
           null
       }
     </div>
   );
 }
-
+    
+    
 export default App;
--- reacterontend/src/Components/EditArtist.js
+++ reacterontend/src/Components/EditArtist.js
@@ -2,8 +2,11 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-function EditArtist() {
-    const [show, setShow] = useState(false);
+function EditArtist(props) {
+    const [name ,  setName] = useState(props.name);
+    const [genre ,  setGenre] = useState(props.genre);
+
+    const [show ,  setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
@@ -25,7 +28,15 @@ function EditArtist() {
                     <Modal.Title>Update Artist</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <form id="editmodal" className="w-full max-w-sm">
+                    TODO: Create fetch request to update artist
+                    <form onSubmit={(e) => {
+                        e.preventDefault();
+                        console.log("Update Artist from editArtist");
+                        console.log(props.id, name, genre);
+                        props.updateArtist(props.id, name, genre);
+                        handleClose();
+                    }}
+                          id="editmodal" className="w-full max-w-sm">
                         <div className="md:flex md:items-center mb-6">
                             <div className="md:w-1/3">
                                 <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
@@ -36,7 +47,10 @@ function EditArtist() {
                             <div className="md:w-2/3">
                                 <input
                                     className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
-                                    id="name" type="text" value="Shining"
+                                    id="name" 
+                                    type="text" 
+                                    value={name}
+                                    onChange={(e) => {setName(e.target.value)}}
                                 />
                             </div>
                         </div>
@@ -51,7 +65,10 @@ function EditArtist() {
                             <div className="md:w-2/3">
                                 <input
                                     className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
-                                    id="genre" type="text" value="Free Jazz"
+                                    id="genre" 
+                                    type="text" 
+                                    value={genre}
+                                    onChange={(e) => {setGenre(e.target.value)}}
                                 />
                             </div>
                         </div>
