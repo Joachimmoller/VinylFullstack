b529d7c712c3896bd44e062204359f858309a43a
Joachimmoller <joachimmoller@gmail.com>
2024-03-19 14:59:54 +0100

    Background added to GUI (FrontEnd)

--- reacterontend/src/App.js
+++ reacterontend/src/App.js
@@ -1,74 +1,17 @@
-import Artist from "./Components/Artist";
-import { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import AddArtist from "./Components/AddArtist";
+import Header from "./components/Header";
+import BackgroundImg from "./assets/record-black-background.jpg"
+import Artists from "./pages/Artists";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
-    
-  const [genre, setGenre] = useState("Rock");
-  const [artists, setArtists] = useState(
-      [
-        {id: 1, name: "Metallica", genre: "Thrash Metal", img: ""},
-        {id: 2, name: "Emperor", genre: "Black Metal", img: ""},
-        {id: 3, name: "Pantera", genre: "Groove Metal", img: ""},
-        {id: 4, name: "Samael", genre: "Industrial Black Metal", img: ""},
-        {id: 5, name: "Opeth", genre: "Progressive Metal", img: ""},
-        {id: 6, name: "Tool", genre: "Progressive Metal", img: ""}
-      ]
-  );
-  function updateArtist(id, newName, newGenre) {
-      const updatedArtists = artists.map(artist => {
-          if(id == artist.id) {
-              return {...artist, name: newName, genre: newGenre};
-          }
-          return artist;
-      });
-          setArtists(updatedArtists);
-      
-  }
-  
-  function newArtist(name, genre) {
-      newArtist = {
-          id: uuidv4(),
-          name: name,
-          genre: genre,
-          img: ""
-      };
-      setArtists([...artists, newArtist]);
-  }
-  const showArtist = true;
   return (
-    <div className="App bg-red-400 min-h-[960px]">
-      {showArtist ?
-          <>
-              <input type="text" onChange={(e) => {
-                  console.log(e.target.value);
-                  setGenre(e.target.value);
-              }}
-              />
-              <p>TODO: Create Header</p>
-              <p>TODO: Create Background</p>
-              <AddArtist newArtist={newArtist}/>
-              <div className="flex flex-wrap justify-center">
-                  {artists.map((artist, index) => {
-                      return (
-                          <Artist key={artist.id}
-                                  id={artist.id}
-                                  name={artist.name}
-                                  genre={artist.genre}
-                                  img={artist.img}
-                                  updateArtist={updateArtist}
-                          />
-                      );
-                  })}
-              </div>
-          </>
-          :
-          null
-      }
-    </div>
-  );
+      <BrowserRouter>
+            <Header>
+                <Routes>
+                    <Route path="/artists" element={<Artists />} />
+                </Routes>
+            </Header>   
+      </BrowserRouter>
+  )
 }
-    
-    
 export default App;
---
+++ reacterontend/src/pages/Artists.js
@@ -0,0 +1,70 @@
+import Artist from "../components/Artist";
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import AddArtist from "../components/AddArtist";
+import EditArtist from "../components/EditArtist";
+import Header from "../components/Header";
+import BackgroundImg from "../assets/record-black-background.jpg"
+
+function Artists() {
+
+    const [genre, setGenre] = useState("Rock");
+    const [artists, setArtists] = useState(
+        [
+            {id: 1, name: "Metallica", genre: "Thrash Metal", img: ""},
+            {id: 2, name: "Emperor", genre: "Black Metal", img: ""},
+            {id: 3, name: "Pantera", genre: "Groove Metal", img: ""},
+            {id: 4, name: "Samael", genre: "Industrial Black Metal", img: ""},
+            {id: 5, name: "Opeth", genre: "Progressive Metal", img: ""},
+            {id: 6, name: "Tool", genre: "Progressive Metal", img: ""}
+        ]
+    );
+    function updateArtist(id, newName, newGenre) {
+        const updatedArtists = artists.map(artist => {
+            if(id == artist.id) {
+                return {...artist, name: newName, genre: newGenre};
+            }
+            return artist;
+        });
+        setArtists(updatedArtists);
+
+    }
+    function newArtist(name, genre) {
+        newArtist = {
+            id: uuidv4(),
+            name: name,
+            genre: genre,
+            img: ""
+        };
+        setArtists([...artists, newArtist]);
+    }
+    const showArtist = true;
+    return (
+        <div className="">
+            {showArtist ?
+                <>
+                    <p>TODO: Create Background</p>
+                    <AddArtist newArtist={newArtist}/>
+                    <div className="flex flex-wrap justify-center">
+                        {artists.map((artist, index) => {
+                            return (
+                                <Artist key={artist.id}
+                                        id={artist.id}
+                                        name={artist.name}
+                                        genre={artist.genre}
+                                        img={artist.img}
+                                        updateArtist={updateArtist}
+                                />
+                            );
+                        })}
+                    </div>
+                </>
+                :
+                null
+            }
+        </div>
+    );
+}
+
+
+export default Artists;
